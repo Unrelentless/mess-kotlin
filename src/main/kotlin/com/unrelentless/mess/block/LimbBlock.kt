1739844bb6cc834e80b8480d22402bc9c8fed331
Unrelentless <p.boryseiko@gmail.com>
2020-10-15 14:04:02 +1100

    With working block entity

--- src/main/kotlin/com/unrelentless/mess/block/LimbBlock.kt
+++ src/main/kotlin/com/unrelentless/mess/block/LimbBlock.kt
@@ -2,14 +2,13 @@ package com.unrelentless.mess.block
 
 import com.unrelentless.mess.util.Level
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
-import net.minecraft.block.Block
-import net.minecraft.block.BlockEntityProvider
-import net.minecraft.block.BlockWithEntity
+import net.minecraft.block.*
 import net.minecraft.block.entity.BlockEntity
 import net.minecraft.world.BlockView
 
-open class LimbBlock(settings: FabricBlockSettings, level: Level): Block(settings.nonOpaque()) {
+open class LimbBlock(settings: FabricBlockSettings, level: Level): BlockWithEntity(settings.nonOpaque()) {
     private val level: Level = level;
 
-//    override fun createBlockEntity(world: BlockView?): BlockEntity? = level.blockEntity()
+    override fun createBlockEntity(world: BlockView?): BlockEntity? = level.blockEntity
+    override fun getRenderType(state: BlockState?): BlockRenderType = BlockRenderType.MODEL
 }
