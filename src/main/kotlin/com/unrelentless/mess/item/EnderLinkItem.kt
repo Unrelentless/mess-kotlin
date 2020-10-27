cbd019f16bad16e5637cffa5692f9b66a1173e99
Unrelentless <p.boryseiko@gmail.com>
2020-10-27 19:10:08 +1100

    Convert all ender eyes if link is broken

--- src/main/kotlin/com/unrelentless/mess/item/EnderLinkItem.kt
+++ src/main/kotlin/com/unrelentless/mess/item/EnderLinkItem.kt
@@ -1,6 +1,7 @@
 package com.unrelentless.mess.item
 
 import com.unrelentless.mess.Mess
+import com.unrelentless.mess.block.entity.BrainBlockEntity
 import com.unrelentless.mess.client.gui.screen.MessScreen
 import com.unrelentless.mess.entity.EnderLinkEntity
 import com.unrelentless.mess.mixin.EyeOfEnderEntityAccessor
@@ -11,12 +12,15 @@ import net.minecraft.client.item.TooltipContext
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
 import net.minecraft.sound.SoundCategory
 import net.minecraft.sound.SoundEvents
 import net.minecraft.text.Text
+import net.minecraft.text.TranslatableText
 import net.minecraft.util.Hand
 import net.minecraft.util.Identifier
 import net.minecraft.util.TypedActionResult
+import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 
 
@@ -32,7 +36,7 @@ class EnderLinkItem : Item(enderLinkItemSettings) {
         return if(world.isClient)
             TypedActionResult.fail(user.getStackInHand(hand))
         else
-            openScreen(world, user)
+            openScreenIfPossible(world, user)
     }
 
     override fun appendTooltip(stack: ItemStack, world: World?, tooltip: MutableList<Text>, context: TooltipContext) {
@@ -42,19 +46,44 @@ class EnderLinkItem : Item(enderLinkItemSettings) {
         }
     }
 
-    private fun openScreen(
+    private fun openScreenIfPossible(
             world: World,
             player: PlayerEntity
     ): TypedActionResult<ItemStack> {
-        val itemStack = player.mainHandStack
+        if(world.isClient) return TypedActionResult.consume(player.mainHandStack)
 
-        if(world.isClient) return TypedActionResult.consume(itemStack)
+        val itemStack = player.mainHandStack
         val blockPos = itemStack.deserializeBrainBlockPos() ?: return TypedActionResult.fail(itemStack)
 
-        val enderLinkEntity = EnderLinkEntity(world, player.x, player.getBodyY(0.5), player.z) {
-            MessScreen.openScreen(world, blockPos, player)
+        spawnEnderEntity(world, player, itemStack, blockPos) {
+            if(world.getBlockEntity(blockPos) !is BrainBlockEntity) {
+                player.inventory.main.forEachIndexed { index, itemStack ->
+                    if(itemStack.item == ITEM && itemStack.deserializeBrainBlockPos() == blockPos) {
+                        player.inventory.main[index] = ItemStack(Items.ENDER_EYE, itemStack.count)
+                        player.inventory.markDirty()
+                    }
+                }
+
+                player.sendMessage(TranslatableText("message." + Mess.IDENTIFIER + ".link_broken"), false)
+            } else{
+                MessScreen.openScreen(world, blockPos, player)
+            }
         }
 
+        if (!player.abilities.creativeMode) itemStack.decrement(1)
+        player.swingHand(player.activeHand, true)
+
+        return TypedActionResult.success(itemStack)
+    }
+
+    private fun spawnEnderEntity(
+            world: World,
+            player: PlayerEntity,
+            itemStack: ItemStack,
+            blockPos: BlockPos,
+            handler: EnderLinkEntity.OnDestroyHandler
+    ) {
+        val enderLinkEntity = EnderLinkEntity(world, player.x, player.getBodyY(0.5), player.z, handler)
         enderLinkEntity.setItem(itemStack)
         enderLinkEntity.initTargetPos(blockPos)
         (enderLinkEntity as EyeOfEnderEntityAccessor).setDropsItem(false)
@@ -70,10 +99,5 @@ class EnderLinkItem : Item(enderLinkItemSettings) {
                 0.5f,
                 0.4f / (RANDOM.nextFloat() * 0.4f + 0.8f)
         )
-
-        if (!player.abilities.creativeMode) itemStack.decrement(1)
-        player.swingHand(player.activeHand, true)
-
-        return TypedActionResult.success(itemStack)
     }
 }
